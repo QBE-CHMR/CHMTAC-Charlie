2c30bf4ffaf9b7dc640a9ecc36855a8b73291445
Marshall Hageman <marshallhageman@gmail.com>
2025-04-28 09:27:45 -0500

    remove explicit port definition

--- chmr-dmz-dal/server/server.js
+++ chmr-dmz-dal/server/server.js
@@ -23,6 +23,6 @@ app.use('/report/management', managementRouter);
 
 const PORT = process.env.Port || 5000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
